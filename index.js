e7c4c82147a7aaedd48e873578831e8cf9d79284
erickpjoshy <erickpjoshy@gmail.com>
2024-06-25 21:46:27 +0530

    2 commit

--- index.js
+++ index.js
@@ -5,13 +5,52 @@ import router from './routes/routes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const app = express();
 
+// CORS middleware setup
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'https://backend.testing4.xyz/',
+  'http://backend.testing4.xyz/',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (
+      !origin ||
+      allowedOrigins.includes(origin) ||
+      process.env.NODE_ENV === 'development'
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.get('/', (req, res) => {
+  res.status(200).json('Service started');
+});
+
+// middlewares
 app.use(express.json());
-app.use(cors());
-app.use(express.static('uploads'));
-//changed
+
 app.use(router);
-app.listen(3000, () => {
-  console.log('App is Running at @ http://localhost:3000');
+app.use(express.static('uploads'));
+app.set('view engine', 'ejs');
+
+// Catch-all for invalid routes
+app.use('*', (req, res) => {
+  console.log('invalid link');
+  res.status(404).send('Invalid link');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`app is running @ http://localhost:${PORT}/`);
 });
